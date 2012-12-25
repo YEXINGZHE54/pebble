b65e901edee6d1ff6a46656405d47a0487c296c3
lizhaoxing <ispe54@gmail.com>
2012-12-26 00:02:52 +0800

    config file; threads, mysql user and pwd

--- ini.c
+++ ini.c
@@ -28,11 +28,11 @@ int config_handler(void *user, const char *section, const char *name, const char
     configuration* pconfig = g->config;
 
     #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
-    if (MATCH("protocol", "version")) {
+    if (MATCH("global", "thread")) {
         pconfig->threads = atoi(value);
-    } else if (MATCH("user", "name")) {
+    } else if (MATCH("mysql", "name")) {
         pconfig->name = strdup(value);
-    } else if (MATCH("user", "pwd")) {
+    } else if (MATCH("mysql", "pwd")) {
         pconfig->pwd = strdup(value);
     } else {
         return 0;  /* unknown section/name, error */
